dbba07d87e6210ad4cb9620bea90e83000057aed
Carlos Augusto Castellano <carlos.aaoc100907@gmail.com>
2026-07-31 22:16:43 -0300

    Começando a atividade

---
+++ Tarefa01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Tarefa01 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int numero = 0;
+        int palpite = 0;
+
+        System.out.println("Tente adivinhar o númnero secreto"); //* teste */
+    }
+}
